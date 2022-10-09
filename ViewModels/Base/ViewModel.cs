4e85bac1fdd61aafad75be5793802feebae77003
Дмитрий Шошин <shoshin.142@yandex.ru>
2022-10-10 01:14:59 +0300

    Pattern MVVM

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            else
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Project.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+    }
+}
